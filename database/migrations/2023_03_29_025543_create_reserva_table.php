d64488b36e4eb9397ba66f7541e218a203b9b08a
melrodriguez1133 <202000231@est.umss.edu>
2023-03-28 23:24:46 -0400

    creacion tablas

---
+++ database/migrations/2023_03_29_025543_create_reserva_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReservaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reserva', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('fecha_hora_solicitud');
+            $table->dateTime('fecha_hora_aprobacion');
+            $table->string('estado');
+            $table->foreignId('id_cliente')
+                  ->constrained('cliente')
+                  ->cascadeOnUpdate()
+                  ->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reserva');
+    }
+}
