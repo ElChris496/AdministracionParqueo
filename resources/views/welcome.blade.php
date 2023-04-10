a96500387216e02c06d20983fb26e626de068fd5
QTMChris <202000653@est.umss.edu>
2023-04-10 03:37:09 -0400

    "Github rut"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema-Parqueo</title>
-    <link href="{{ secure_asset('resources\css\menustyle.css') }}" media="all" rel="stylesheet" type="text/css" />
+    <link href="{{ secure_asset('\resources\css\menustyle.css') }}" media="all" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
@@ -14,7 +14,7 @@
             <header>
                 <div class="container">
                     <div class="logo">
-                    <img src="/resources/img/logo.png" alt="logo">
+                    <img src="https://github.com/ElChris496/AdministracionParqueo/blob/master/resources/img/logo.png" alt="logo">
                     <h3>Parqueo</h3>
                     </div>
                 <div class="links">
@@ -43,7 +43,7 @@
                     </div>
                 </div>
                 <div class="rigth">
-                    <img src="/resources/img/auto.png" alt="car img"class="car">
+                    <img src="https://github.com/ElChris496/AdministracionParqueo/blob/master/resources/img/auto.png" alt="car img"class="car">
                 </div>
                 </div>
             </div>
