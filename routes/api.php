99a662170e91be1aab4488f5e5a750d6b9263440
melrodriguez1133 <202000231@est.umss.edu>
2023-04-09 22:52:58 -0400

    cambio api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\clienteController;
+use App\Http\Controllers\loginController;
 use App\Http\Controllers\parqueoController;
 /*
 |--------------------------------------------------------------------------
