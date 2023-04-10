51f1a8e980e8b36cd39f38bbdc92ac535963f08f
QTMChris <202000653@est.umss.edu>
2023-04-10 03:53:19 -0400

    "Rutas"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema-Parqueo</title>
-    <link href="https://administracionparqueo-production.up.railway.app/resources\css\menustyle.css" media="all" rel="stylesheet" type="text/css">
+    <link href="/resources\css\menustyle.css" media="all" rel="stylesheet" type="text/css">
 </head>
 
 <body>
