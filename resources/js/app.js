eecbf7be71541ca27d3bbc96ace62713e55c3581
sergio60711611 <Sergio_01_gabriel@hotmail.com>
2023-05-07 17:35:32 -0400

    prueba 2

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,11 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+// Use PORT provided in environment or default to 3000
+var port = process.env.PORT || 3000;
+
+// Listen on `port` and 0.0.0.0
+app.listen(port, '0.0.0.0', function () {
+  // ...
+});
